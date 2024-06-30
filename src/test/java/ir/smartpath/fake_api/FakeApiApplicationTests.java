26935e9ef2bccd0bf7687833af7bf416ad866dfc
soheil babadi <babadishyl@gmail.com>
2024-06-30 17:37:25 +0330

    init commit

---
+++ src/test/java/ir/smartpath/fake_api/FakeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ir.smartpath.fake_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
