0aba50ad59641bb973cabd40c8120833dccb1ff1
soheil babadi <babadishyl@gmail.com>
2024-07-01 12:44:12 +0330

    test docker container

--- src/main/java/ir/smartpath/fake_api/controller/FakeController.java
+++ src/main/java/ir/smartpath/fake_api/controller/FakeController.java
@@ -1,16 +1,24 @@
 package ir.smartpath.fake_api.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
 @RestController
-@RequestMapping("/fake")
+@RequestMapping("/api/v1/fake")
+@Slf4j
 public class FakeController {
 
-        @GetMapping("/hello")
-        public ResponseEntity<String> hello() {
-            return ResponseEntity.ok("Hello World");
-        }
+    @GetMapping
+    public ResponseEntity<String> hello() {
+
+        String response="This is a fake api running on port 7000 on " + LocalDateTime.now(ZoneId.of("UTC"));
+        log.info(response);
+        return ResponseEntity.ok(response);
+    }
 }
